ece690ce1f760b48fa1aac276c7584e5fadeca20
Beata Knecht <beatamknecht@gmail.com>
2022-11-06 20:28:53 -0500

    made a function!

--- tests/testthat.R
+++ tests/testthat.R
@@ -10,3 +10,19 @@ library(testthat)
 library(myspotify)
 
 test_check("myspotify")
+
+library(tidyverse)
+
+
+
+most_played_artist <- function(temp){
+  sorted_table <- temp %>%
+    count(master_metadata_album_artist_name) %>%
+    arrange(desc(n))
+  most_played <- sorted_table[1,1]
+  print(paste("Your most played artist of all time is ", most_played, "!", sep=""))
+}
+
+
+
+
