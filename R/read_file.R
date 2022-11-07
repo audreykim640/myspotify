3aaf6e0297c64022c2e8408d9d944f7335b715f7
Beata Knecht <beatamknecht@gmail.com>
2022-11-06 19:12:37 -0500

    uploaded data

--- R/read_file.R
+++ R/read_file.R
@@ -7,3 +7,13 @@ function(filepath) {
 
 temp <- jsonlite::fromJSON(txt = 'my_spotify_data/MyData/endsong.json', flatten = "true")
 # data <- as.data.frame(temp)
+
+function(filepath){
+  jsonlite::fromJSON(txt = 'beata_spotify_data/SearchQueries.json', flatten = "true" )
+}
+Search <- jsonlite::fromJSON(txt = 'beata_spotify_data/SearchQueries.json', flatten = "true")
+
+function(filepath){
+  jsonlite::fromJSON(txt = 'beata_spotify_data/YourLibrary.json', flatten = "true" )
+}
+library <- jsonlite::fromJSON(txt = 'beata_spotify_data/YourLibrary.json', flatten = "true")
