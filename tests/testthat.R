bfd6ef86deebebb5539158e53c53260e8c7aef49
Beata Knecht <beatamknecht@gmail.com>
2022-11-08 10:31:06 -0500

    edited my function

--- tests/testthat.R
+++ tests/testthat.R
@@ -19,7 +19,9 @@ most_played_artist <- function(temp){
     count(master_metadata_album_artist_name) %>%
     arrange(desc(n))
   most_played <- sorted_table[1,1]
-  print(paste("Your most played artist of all time is ", most_played, "!", sep=""))
+  second_played <- sorted_table[2, 2]
+  third_played <- sorted_table[3, 1]
+  print(paste("Your most played artist of all time is ", most_played, "! Followed by ", second_played, " and ", third_played,  sep=""))
 }
 
 
