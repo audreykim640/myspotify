28ffcf1f1aa545068e64a79b2e38958b7d7bde0a
Audrey Kim <akim89@smith.edu>
2022-11-08 10:37:18 -0500

    Adding example of extended data set

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,10 @@
 ## code to prepare `DATASET` dataset goes here
 
 usethis::use_data(DATASET, overwrite = TRUE)
+
+# [Template above; actual code below]--------------------------
+
+## code to prepare `extended_example` dataset goes here
+extended_example <- jsonlite::fromJSON(txt = "data-raw/extendeddata_example.json", flatten = TRUE)
+
+usethis::use_data(extended_example, overwrite = TRUE)
