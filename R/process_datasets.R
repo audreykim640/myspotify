23bed2eae812410cce57aff48065a77da78e1757
Audrey Kim <akim89@smith.edu>
2022-11-08 10:39:52 -0500

    Starting dataset documentation

---
+++ R/process_datasets.R
@@ -0,0 +1,16 @@
+
+#' Example of personal data (extended version) downloaded from Spotify
+#'
+#' Listening history data from Spotify including song/podcast information
+#' and interaction with given songs
+#'
+#' @format ## `who`
+#' A data frame with 7,240 rows and 60 columns:
+#' \describe{
+#'   \item{country}{Country name}
+#'   \item{iso2, iso3}{2 & 3 letter ISO country codes}
+#'   \item{year}{Year}
+#'   ...
+#' }
+#' @source <https://www.who.int/teams/global-tuberculosis-programme/data>
+"who"
