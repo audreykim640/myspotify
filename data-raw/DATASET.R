cd18ae4a1476b10265ec7b609e0f7dabca2aa9b7
Beata Knecht <beatamknecht@gmail.com>
2022-11-10 10:04:28 -0500

    built function to wrangle extended data

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -5,6 +5,9 @@ usethis::use_data(DATASET, overwrite = TRUE)
 # [Template above; actual code below]--------------------------
 
 ## code to prepare `extended_example` dataset goes here
-extended_example <- jsonlite::fromJSON(txt = "data-raw/extendeddata_example.json", flatten = TRUE)
+extended_example <- jsonlite::fromJSON(txt = "data-raw/extendeddata_example.json", flatten = TRUE) %>%
+  rename(song = master_metadata_track_name, artist = master_metadata_album_artist_name,
+         album = master_metadata_album_album_name) %>%
+  select(ts, ms_played, song, artist, album, reason_start, reason_end)
 
 usethis::use_data(extended_example, overwrite = TRUE)
