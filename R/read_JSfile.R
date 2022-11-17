741b33ca07e00f113904f3dfc62cc55162f52a9e
Audrey Kim <akim89@smith.edu>
2022-11-17 09:48:48 -0500

    Renaming file reader function

--- R/read_JSfile.R
+++ R/read_JSfile.R
@@ -1,4 +1,4 @@
-#' Read-in Spotify data file
+#' Read-in Spotify JSON data file
 #'
 #' @param filepath File path string to where the endsong.json file is hosted
 #'
@@ -7,12 +7,16 @@
 #' @import dplyr
 #' @import jsonlite
 #'
-#' @examples readfile()
+#' @examples read_JSfile()
 readfile <- function(filepath) {
   jsonlite::fromJSON(txt = filepath, flatten = TRUE) %>%
     rename(song = master_metadata_track_name,
            artist = master_metadata_album_artist_name,
            album = master_metadata_album_album_name) %>%
     select(ts, ms_played, song, artist, album, reason_start, reason_end) %>%
-    mutate(ts = as.POSIXct(ts, format = "%Y-%m-%dT%H:%M:%OSZ"))
+    tidyr::separate(col = ts, into = c("date", "time"), sep = "T") %>%
+    mutate(date = as.POSIXct(date, format = "%Y-%m-%d"),
+           time = as.POSIXct(substr(time, start = 1, stop = 8), format = "%H:%M:%S"),
+           time = lubridate::force_tz(time, tzone = 'GMT'),
+           year = substr(date, 1, 4))
 }
