d42b8ca8fc973056fda1b6e76e102b6c4040314d
Audrey Kim <akim89@smith.edu>
2022-11-17 16:19:13 -0500

    Function to check for excluded items

---
+++ R/DNE_in.R
@@ -0,0 +1,15 @@
+#' List matching -- return items that don't exist
+#'
+#' @param baseline List of items to compare against
+#' @param checkfor List of items to check for in baseline
+#'
+#' @return Returns list of items in "checkfor" that do NOT exist in "baseline"
+#' @export
+#'
+#' @examples
+DNE_in <- function(baseline, checkfor) {
+  bools <- checkfor %in% baseline
+  index <- which(bools == FALSE)
+  itemsDNE <- checkfor[index]
+  itemsDNE
+}
--- R/contains.R
+++
@@ -1,9 +0,0 @@
-# list --> original thing we want to check against
-# checkfor --> see if these items exist in list
-
-# contains <- function(list, checkfor = c("")) {
-#   df_names <- colnames(df)
-#   for userinput in cols {
-#     if
-#   }
-# }
