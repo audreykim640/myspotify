d48630cb4a0b4a6de2f8b5ee614f315306892dd0
Audrey Kim <akim89@smith.edu>
2022-11-17 20:10:14 -0500

    Updating data cleaning procedures

--- R/read_file.R
+++ R/read_file.R
@@ -15,8 +15,11 @@ readfile <- function(filepath) {
            album = master_metadata_album_album_name) %>%
     select(ts, ms_played, song, artist, album, reason_start, reason_end) %>%
     tidyr::separate(col = ts, into = c("date", "time"), sep = "T") %>%
-    mutate(date = as.POSIXct(date, format = "%Y-%m-%d"),
+    mutate(date = as.Date(date, format = "%Y-%m-%d"), # should remain as.Date() for plot_dates() function
            time = as.POSIXct(substr(time, start = 1, stop = 8), format = "%H:%M:%S"),
-           time = lubridate::force_tz(time, tzone = 'GMT'),
-           year = substr(date, 1, 4))
+           time = lubridate::force_tz(time, tzone = "GMT"),
+           year = lubridate::year(date),
+           month = lubridate::month(date),
+           dayyear = date,
+           monthyear = paste(lubridate::month(date), lubridate::year(date)))
 }
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -12,10 +12,13 @@ extended_example <- jsonlite::fromJSON(txt = "data-raw/extended_example.json", f
          album = master_metadata_album_album_name) %>%
   select(ts, ms_played, song, artist, album, reason_start, reason_end) %>%
   tidyr::separate(col = ts, into = c("date", "time"), sep = "T") %>%
-  mutate(date = as.POSIXct(date, format = "%Y-%m-%d"),
+  mutate(date = as.Date(date, format = "%Y-%m-%d"), # should remain as.Date() for plot_dates() function
          time = as.POSIXct(substr(time, start = 1, stop = 8), format = "%H:%M:%S"),
          time = lubridate::force_tz(time, tzone = "GMT"),
-         year = substr(date, 1, 4))
+         year = lubridate::year(date),
+         month = lubridate::month(date),
+         dayyear = date,
+         monthyear = paste(lubridate::month(date), lubridate::year(date)))
 
  # mutate(ts = as.POSIXct(ts, format = "%Y-%m-%dT%H:%M:%OSZ"))
 
