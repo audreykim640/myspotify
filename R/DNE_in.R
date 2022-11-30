c7590aad0a72774360f118bc1a3a67d1c39dbaee
Audrey Kim <akim89@smith.edu>
2022-11-30 08:36:42 -0500

    Adding example, removing export (for internal use)

--- R/DNE_in.R
+++ R/DNE_in.R
@@ -4,9 +4,14 @@
 #' @param checkfor List of items to check for in baseline
 #'
 #' @return Returns list of items in "checkfor" that do NOT exist in "baseline"
-#' @export
 #'
 #' @examples
+#'
+#' checking <- c("apple", "banana", "clementine")
+#' for <- c("apple", "peach", "banana")
+#' DNE_in(checking, for)
+#' #> returns "peach"
+#'
 DNE_in <- function(baseline, checkfor) {
   bools <- checkfor %in% baseline
   index <- which(bools == FALSE)
