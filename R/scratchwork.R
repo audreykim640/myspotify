6ed7466254e53cefb361f9ae912a22503766e0b6
Audrey Kim <akim89@smith.edu>
2022-11-30 08:35:29 -0500

    Rename file

--- R/scratchwork.R
+++ R/scratchwork.R
@@ -30,3 +30,17 @@
 #
 #
 #
+
+# Plot top artists over time
+
+# plot_dates <- function(num_artists) {
+#   dates <- temp %>%
+#     mutate(date = as.Date(substr(ts, 1, 10), "%Y-%m-%d"))
+#
+#   ggplot(dates, aes(x = date)) +
+#     geom_bar()  +
+#     scale_x_date(date_breaks = "1 month", date_labels = "%b %Y",
+#                  limit=c(as.Date("2020-01-01"),as.Date("2020-02-11"))
+#     ) +
+#     theme(axis.text.x = element_text(angle = -90, vjust = 0.5))
+# }
