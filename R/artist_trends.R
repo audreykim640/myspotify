b0d261f8a2a87b06e44c6b0a0ebb55ff86ad16af
Audrey Kim <akim89@smith.edu>
2022-12-01 10:35:14 -0500

    Housekeeping

---
+++ R/artist_trends.R
@@ -0,0 +1,30 @@
+#' Plot top artists over time
+#'
+#' @param df Cleaned dataframe (using myspotify::read_file()) of Spotify listening history
+#' @param num Number of top artists to plot per year
+#'
+#' @return ggplot2 plot of the top artists listened to over the years
+#' @export
+#' @import dplyr
+#' @import ggplot2
+#'
+#' @examples
+#' df <- myspotify$extended_example
+#'
+#' artist_trends(df, num = 5) +
+#' labs(subtitle = "Top 5")
+artist_trends <- function(df, num = 3) {
+  df <- df %>%
+    count(year, artist) %>%
+    group_by(year) %>%
+    arrange(desc(n)) %>%
+    slice(1:num)
+
+  ggplot(df, aes(x = year, y = n, color = artist)) +
+    geom_text(aes(label = artist)) +
+    labs(title = "Top artists over the years",
+         x = "Year",
+         y = "Number of listens") +
+    theme_minimal() +
+    theme(legend.position="none")
+}
