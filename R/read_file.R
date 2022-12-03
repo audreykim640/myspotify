db0f6504ed41d3cae6cb08c4095fdcc7ac7e0a75
Audrey Kim <akim89@smith.edu>
2022-12-03 11:44:29 -0500

    Trying to allow multiple inputs

--- R/read_file.R
+++ R/read_file.R
@@ -8,21 +8,28 @@
 #' @import lubridate
 #' @importFrom jsonlite fromJSON
 #' @importFrom tidyr separate
+#' @importFrom purrr map
 #'
 #' @examples
-#' read_file("C:/Users/SophiaSmith/Desktop/my_spotify_data/MyData/endsong_4.json")
 #'
-read_file <- function(filepath) {
+#' # not sure how to write function for this
+#'
+read_file <- function(...) {
+  single_file <- function(filepath) {
     df <- fromJSON(txt = filepath, flatten = TRUE) %>%
-    rename(song = master_metadata_track_name,
-           artist = master_metadata_album_artist_name,
-           album = master_metadata_album_album_name) %>%
-    select(ts, ms_played, song, artist, album, reason_start, reason_end) %>%
-    separate(col = ts, into = c("date", "time"), sep = "T") %>%
-    mutate(date = as.Date(date, format = "%Y-%m-%d"), # should remain as.Date() for plot_dates() function
-           time = as.POSIXct(substr(time, start = 1, stop = 8), format = "%H:%M:%S"),
-           time = lubridate::force_tz(time, tzone = "GMT"),
-           year = lubridate::year(date),
-           month = factor(month.name[lubridate::month(date)],levels=month.name))
+      rename(song = master_metadata_track_name,
+             artist = master_metadata_album_artist_name,
+             album = master_metadata_album_album_name) %>%
+      select(ts, ms_played, song, artist, album, reason_start, reason_end) %>%
+      separate(col = ts, into = c("date", "time"), sep = "T") %>%
+      mutate(date = as.Date(date, format = "%Y-%m-%d"), # should remain as.Date() for plot_dates() function
+             time = as.POSIXct(substr(time, start = 1, stop = 8), format = "%H:%M:%S"),
+             time = lubridate::force_tz(time, tzone = "GMT"),
+             year = lubridate::year(date),
+             month = factor(month.name[lubridate::month(date)],levels=month.name))
     df
+  }
+
+  rbind(map(..., single_file))
+
 }
