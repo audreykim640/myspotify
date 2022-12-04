315295815c78610f0985b25d35eea1f70bed5073
Beata Knecht <beatamknecht@gmail.com>
2022-12-04 13:54:27 -0500

    started documentation on search_word()

--- R/search_word.R
+++ R/search_word.R
@@ -1,7 +1,17 @@
-search_word <- function(df, word) {
+#' Search specific word or string in song name
+#'
+#' @param df
+#' @param string
+#'
+#' @return dataframe of a list of all the songs a user streamed with that specific string in the song name
+#' @export
+#'
+#' @examples
+#' search_word(extended_example, love)
+search_word <- function(df, string) {
   sorted_table <- df %>%
     select(song, artist, album) %>%
     distinct() %>%
-    filter(grepl(word, song, ignore.case = TRUE))
+    filter(grepl(string, song, ignore.case = TRUE))
   sorted_table
 }
