de411e953a74356426c83568ba6d29fc8760ef37
Beata Knecht <beatamknecht@gmail.com>
2022-12-05 18:46:49 -0500

    edited roxygen skeleton for search_word

--- R/search_word.R
+++ R/search_word.R
@@ -1,10 +1,11 @@
 #' Search specific word or string in song name
 #'
-#' @param df
-#' @param string
+#' @param df Cleaned dataframe (using myspotify::read_file) of Spotity listening history
+#' @param string by The feature by which to check for skips (e.g., song, artist)
 #'
 #' @return dataframe of a list of all the songs a user streamed with that specific string in the song name
 #' @export
+#' @import dplyr
 #'
 #' @examples
 #' search_word(extended_example, love)
